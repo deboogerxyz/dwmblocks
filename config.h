2024eaab88991a0c659844a831dbdd7a618f275b
debooger <debooga@gmail.com>
2021-05-14 19:31:13 +0200

    Added music status bar module

--- config.h
+++ config.h
@@ -1,6 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"",		"sbmusic",		5,			17},
 	{"",		"sbforecast",		60,			16},
 	{"",		"sbdisk",		30,			15},
 	{"",		"sbmemory",		10,			14},
